ee2f49e39b35130902e48c6c25d82f86939b4202
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-07-13 09:23:38 +0900

    [INIT] Projec Inital

---
+++ src/test/java/kr/hs/dgsw/mmr/MmrApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.mmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
